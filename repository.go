8b7d73381166c295b4918c7e90eea4c16f513404
Tyler Darnell <tyler.a.darnell@gmail.com>
2022-09-05 11:41:33 -0500

    feat: add ShortLink data model and repository

---
+++ repository.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tydar/smallurl-ccask/ccask"
+)
+
+type ShortLinkRepository interface {
+	GetLink(key string) (ShortLink, error)
+	SetLink(key, url string) error
+}
+
+type ShortLinkModel struct {
+	client *ccask.CCaskClient
+}
+
+func NewShortLinkModel(client *ccask.CCaskClient) *ShortLinkModel {
+	return &ShortLinkModel{client}
+}
+
+type ShortLink struct {
+	Key string
+	URL string
+}
+
+func (slm *ShortLinkModel) GetLink(key string) (ShortLink, error) {
+	res, err := slm.client.GetRes([]byte(key))
+	if err != nil {
+		return ShortLink{}, fmt.Errorf("client.GetRes: %v", err)
+	}
+
+	switch res.ResCode() {
+	case ccask.GET_SUCCESS:
+		retStr := string(res.Value())
+		return ShortLink{Key: key, URL: retStr}, nil
+	case ccask.GET_FAIL:
+		return ShortLink{}, nil
+	default:
+		return ShortLink{}, fmt.Errorf("Invalid response type from database: %v\n", res.ResCode())
+	}
+}
+
+func (slm *ShortLinkModel) SetLink(key, url string) error {
+	res, err := slm.client.SetRes([]byte(key), []byte(url))
+	if err != nil {
+		return fmt.Errorf("client.SetRes: %v", err)
+	}
+
+	switch res.ResCode() {
+	case ccask.SET_FAIL:
+		return fmt.Errorf("SET request failed; please try again.")
+	case ccask.SET_SUCCESS:
+		return nil
+	default:
+		return fmt.Errorf("Unexpected response code %d; please try again.", res.ResCode())
+	}
+}
